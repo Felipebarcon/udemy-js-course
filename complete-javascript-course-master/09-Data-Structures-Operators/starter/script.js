264a2908ee2ab0197c052e48a3f59052b9356df3
Felipe Barcon <felipe.barcon@gmail.com>
2022-08-21 00:15:55 +0200

    destructuring arrays

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,47 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4, 5];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+const d = arr[3];
+
+// Destructuring array
+const [w, x, y, z] = arr;
+console.log(w, x, y, z);
+
+// Taking first and second value of categories
+//const [first, second] = restaurant.categories;
+
+// Taking first and third value of categories
+/*const [first, , second] = restaurant.categories;
+console.log(first, second);*/
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching variables
+/*const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary);*/
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+/*const [i, , j] = nested;
+console.log(i, j);*/
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
