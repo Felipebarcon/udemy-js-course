93bc821051a0e4bd61806413da1b08499e143915
Felipe Barcon <felipe.barcon@gmail.com>
2022-08-25 23:37:04 +0200

    optional chaining

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -56,11 +56,41 @@ const restaurant = {
 
 /*
 ///////////////////////////////////////////////////////////
-Enhance Objects Literals
+Optional Chaining
 ////////////////////////////////////////////////////////
 */
 
-console.log('------------ Enhance Objects Literals -------------');
+console.log('------------ Optional Chaining -------------');
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example loop on the array and check if the restaurant is open or not
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRistotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Felipe', email: 'hello@felipe.io' }];
+console.log(users[0]?.name ?? 'User array empty');
+
+// Same code than up but more complicated
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
 
 /*
 ///////////////////////////////////////////////////////////
