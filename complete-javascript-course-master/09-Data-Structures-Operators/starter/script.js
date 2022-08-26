6d23eedc5f9b24fa1695929a288587c62883c237
Felipe Barcon <felipe.barcon@gmail.com>
2022-08-26 21:46:07 +0200

    maps first section

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -56,6 +56,45 @@ const restaurant = {
 
 /*
 ///////////////////////////////////////////////////////////
+MAPS
+////////////////////////////////////////////////////////
+*/
+console.log('------------ MAPS -------------');
+
+const rest = new Map();
+rest.set('name', 'Ponte Vechio');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arrBis = [1, 2];
+rest.set(arrBis, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arrBis));
+
+/*
+///////////////////////////////////////////////////////////
 SETS
 ////////////////////////////////////////////////////////
 */
