93550c08f1e2a7b0dd5cc1b92ed81a3605ec2a66
Felipe Barcon <felipe.barcon@gmail.com>
2022-08-26 22:53:55 +0200

    maps section 2

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -56,6 +56,47 @@ const restaurant = {
 
 /*
 ///////////////////////////////////////////////////////////
+MAPS ITERATION
+////////////////////////////////////////////////////////
+*/
+
+console.log('------------ MAPS ITERATION -------------');
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 👏'],
+  [false, 'Try again 😭'],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours)); // same structure that the MAP
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer?'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert MAP to ARRAY
+console.log([...question]);
+console.log([...question.values()]);
+console.log(...question.keys());
+
+/*
+///////////////////////////////////////////////////////////
 MAPS
 ////////////////////////////////////////////////////////
 */
