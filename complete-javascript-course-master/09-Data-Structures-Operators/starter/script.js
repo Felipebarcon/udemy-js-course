030f5fbb172811286e3cec16a8339fe439121cdb
Felipe Barcon <felipe.barcon@gmail.com>
2022-08-28 22:21:33 +0200

    string part 3

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -56,6 +56,59 @@ const restaurant = {
 
 /*
 ///////////////////////////////////////////////////////////
+STRINGS PART 3
+////////////////////////////////////////////////////////
+*/
+console.log('------------ STRINGS PART 3 -------------');
+
+// Split and Join
+console.log('a+very+nice+string'.split('+'));
+console.log('Felipe Barcon'.split(' '));
+
+const [firstName, lastName] = 'Felipe Barcon'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('felipe barcon');
+
+// Padding
+const message = 'Go to gate 23';
+console.log(message.padStart(25, '+').padEnd(35, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + ''; // converting a number into a string
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(444344343434));
+console.log(maskCreditCard('405896656565556'));
+
+// Repeat
+const message2 = 'Bad weather... All departures delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`Ther are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+///////////////////////////////////////////////////////////
 STRINGS PART 2
 ////////////////////////////////////////////////////////
 */
