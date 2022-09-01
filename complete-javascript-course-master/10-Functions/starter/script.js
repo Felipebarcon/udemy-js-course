2c697309335b1cc8b364c0a496f4a62595148e2d
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-01 09:11:14 +0200

    functions default parameter

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,37 @@
-'use strict'
+'use strict';
+
+// DEFAULT PARAMETERS
+
+console.log('---------Default Parameters---------');
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  /*
+  ES5 WAY
+  numPassengers = numPassengers || 1;
+  price = price || 199;
+  */
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH345', 2, 899);
+createBooking('LH567', 5);
+
+createBooking('LH789', undefined, 1000);
+
+// DEFAULT PARAMETERS
+
+console.log('---------Default Parameters---------');
