5272636a060d754be929368c062844c36765f13f
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-05 15:29:47 +0200

    The bind method

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -92,7 +92,7 @@ transformer('JavaScript is the best!', oneWord); // we are only passing the valu
 const high5 = function () {
   console.log('🤟');
 };
-document.body.addEventListener('click', high5); // high5 ==> callback function
+//document.body.addEventListener('click', high5); // high5 ==> callback function
 
 ['Felipe', 'Marta', 'Adam'].forEach(high5);
 
@@ -181,3 +181,43 @@ book.apply(swiss, flightData);
 book.call(swiss, ...flightData); // same as the apply but with spread operator
 
 console.log('---------The Bind Method---------');
+// Bind return a new method where the This will be always defined to Eurowings
+
+// book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Louis Mourot');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application ==> preset parameters
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
