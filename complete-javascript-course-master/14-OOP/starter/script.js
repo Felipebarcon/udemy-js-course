60f655719d5d60368cf492aabb469e25e2598384
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-29 15:10:14 +0200

    object.create

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -163,3 +163,29 @@ console.log(account.movements);
 // Array.from(document.querySelectorAll('h1'));
 
 PersonCl.hey();
+
+///////////////////////////////////////////
+// OBJECT.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 1983;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
