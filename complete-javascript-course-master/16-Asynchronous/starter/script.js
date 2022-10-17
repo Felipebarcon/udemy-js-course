057f9f6081c16aba26b4d9dcec83cc67f8f21cc1
Felipe Barcon <felipe.barcon@gmail.com>
2022-10-17 16:17:05 +0200

    starting promosifying the geolocation api

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -206,7 +206,7 @@ Promise.resolve('Resolved promise 2').then(res => {
 console.log('Test end'); // 2
 
 */
-
+/*
 // BUILDING PROMISES
 
 const lotteryPromise = new Promise(function (resolve, reject) {
@@ -254,3 +254,16 @@ wait(1)
 
 Promise.resolve('abc').then(x => console.log(x));
 Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+*/
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then(pos => console.log(pos));
